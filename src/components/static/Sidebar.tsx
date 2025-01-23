b53cf06122f08b18f12bd08f1585fec2b12a297a
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-23 16:21:53 +0100

    Started sidebar. Everything works fine

--- src/components/static/Sidebar.tsx
+++ src/components/static/Sidebar.tsx
@@ -2,7 +2,18 @@
 
 const Sidebar = () => {
   return (
-    <div className="lgtablet:grid lgtablet:col-span-3">Sidebar</div>
+    <div className="grid grid-cols-1 lgtablet:col-span-3">
+      <div className="bg-brandBlue grid gap-[20px] w-[100%] p-[20px] rounded-[10px]">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div className="grid gap-3">
+          <h2 className="text-absoluteWhite">Joseph Olu</h2>
+          <p className="text-absoluteWhite text-[17px] pb-[10px] border-b-[1px] border-b-brandGrey">Founder of SAAS First - the Best AI and Data-Driven Customer Engagement Tool</p>
+        </div>
+        <p className="text-absoluteWhite text-[14px]">With 11 years in SaaS, I've built MillionVerifier and SAAS First. Passionate about SaaS, data, and AI. Let's connect if you share the same drive for success!</p>
+      </div>
+    </div>
   )
 }
 
