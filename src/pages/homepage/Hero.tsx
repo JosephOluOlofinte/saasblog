e40e4bb198eb4a73a4cd810c675dc02e9d6d875a
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2025-01-24 03:34:10 +0100

    Finished homepage. Everything works fine

--- src/pages/homepage/Hero.tsx
+++ src/pages/homepage/Hero.tsx
@@ -1,36 +1,65 @@
 // import React from 'react'
 
 import { Fragment } from "react/jsx-runtime"
+import blogImg from '../../assets/images/blogimg.png'
+import blogImg2 from '../../assets/images/blogimg2.jpg'
+import blogImg3 from '../../assets/images/blogimg3.jpg'
+import blogImg4 from '../../assets/images/blogimg4.jpg'
+import blogImg5 from '../../assets/images/blogimg5.jpg'
+
+
 
 const Hero = () => {
   return (
     <Fragment>
-      <article className="w-[100%] h-[calc(100vh - 75px)] bg-absoluteWhite rounded-[12px] grid grid-cols-1 laptop:col-span-8 py-[30px] px-[30px]">
-        <h1>Mastering ChatGPT Blog Creation: Dos and Don'ts for SaaS Marketing Managers</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dolor dolorem explicabo distinctio pariatur ratione aut totam consequatur excepturi culpa error iste neque nihil, eaque eos, sunt debitis, enim exercitationem? Laborum explicabo, itaque earum natus magnam voluptatum ducimus molestiae, molestias rerum quisquam ullam non cumque? Omnis temporibus adipisci molestiae quaerat provident eaque ab dicta commodi eius nulla qui sunt perspiciatis, cumque soluta? Obcaecati optio iure voluptates libero blanditiis vel autem dolorum saepe perspiciatis atque? Rem iste obcaecati culpa voluptate, quibusdam a quas ratione asperiores cumque quidem dolore iusto laudantium minus sint quod nihil hic nisi unde eaque voluptatibus commodi. Repellat, numquam fuga. Numquam cupiditate voluptates quidem sequi nesciunt, eligendi soluta, possimus, reiciendis amet id commodi exercitationem sunt. Aspernatur, qui deleniti!</p>
+      <div className="w-[100%] h-fit rounded-[30px] grid gap-[50px] grid-cols-1 laptop:col-span-8 overflow-hidden">
+        
+        <div className="w-[100%] max-h-[450px] bg-absoluteWhite rounded-[30px] grid grid-cols-1 laptop:col-span-8 overflow-hidden relative text-absoluteWhite adb">
+          <img src={blogImg} alt="" />
+          <div className="w-[100%] grid absolute bottom-0 px-[30px] py-[15px] shadow-lg backdrop-blur-md backdrop-saturate-[180%] bg-[rgba(0,0,0,0.3)] ">
+          <a href="" className="flex items-center gap-[10px] p-[5px] w-fit bg-[#F1ECFF] text-[#6C40FF] rounded-3xl mb-[5px]"><div className="h-[16px] w-[16px] bg-[#6C40FF] rounded-full"></div><p>Artificial Intelligence</p></a>
+          <h2 className="text-[1.3rem] lgtablet:text-[1.6rem] leading-7 mb-[5px]">Mastering ChatGPT Blog Creation: Dos and Don'ts for SaaS Marketing Managers</h2>
+          <p className="text-[#cfcece] hidden lgtablet:block">By <a href="">Joseph Olu</a></p>
+        </div>
+        </div>
+        
+        <div className="w-[100%] max-h-[450px] bg-absoluteWhite rounded-[30px] grid grid-cols-1 laptop:col-span-8 overflow-hidden relative text-absoluteWhite">
+          <img src={blogImg2} alt="" />
+          <div className="w-[100%] grid absolute bottom-0 px-[30px] py-[15px] shadow-lg backdrop-blur-md backdrop-saturate-[180%] bg-[rgba(0,0,0,0.3)] ">
+          <a href="" className="flex items-center gap-[10px] p-[5px] w-fit bg-[#F1ECFF] text-[#6C40FF] rounded-3xl mb-[5px]"><div className="h-[16px] w-[16px] bg-[#6C40FF] rounded-full"></div><p>Artificial Intelligence</p></a>
+          <h2 className="text-[1.3rem] lgtablet:text-[1.6rem] leading-7 mb-[5px]">Mastering ChatGPT Blog Creation: Dos and Don'ts for SaaS Marketing Managers</h2>
+          <p className="text-[#cfcece] hidden lgtablet:block">By <a href="">Joseph Olu</a></p>
+        </div>
+        </div>
+        
+        <div className="w-[100%] max-h-[450px] bg-absoluteWhite rounded-[30px] grid grid-cols-1 laptop:col-span-8 overflow-hidden relative text-absoluteWhite">
+          <img src={blogImg3} alt="" />
+          <div className="w-[100%] grid absolute bottom-0 px-[30px] py-[15px] shadow-lg backdrop-blur-md backdrop-saturate-[180%] bg-[rgba(0,0,0,0.3)] ">
+          <a href="" className="flex items-center gap-[10px] p-[5px] w-fit bg-[#F1ECFF] text-[#6C40FF] rounded-3xl mb-[5px]"><div className="h-[16px] w-[16px] bg-[#6C40FF] rounded-full"></div><p>Artificial Intelligence</p></a>
+          <h2 className="text-[1.3rem] lgtablet:text-[1.6rem] leading-7 mb-[5px]">Mastering ChatGPT Blog Creation: Dos and Don'ts for SaaS Marketing Managers</h2>
+          <p className="text-[#cfcece] hidden lgtablet:block">By <a href="">Joseph Olu</a></p>
+        </div>
+        </div>
+        
+        <div className="w-[100%] max-h-[450px] bg-absoluteWhite rounded-[30px] grid grid-cols-1 laptop:col-span-8 overflow-hidden relative text-absoluteWhite">
+          <img src={blogImg4} alt="" />
+          <div className="w-[100%] grid absolute bottom-0 px-[30px] py-[15px] shadow-lg backdrop-blur-md backdrop-saturate-[180%] bg-[rgba(0,0,0,0.3)] ">
+          <a href="" className="flex items-center gap-[10px] p-[5px] w-fit bg-[#F1ECFF] text-[#6C40FF] rounded-3xl mb-[5px]"><div className="h-[16px] w-[16px] bg-[#6C40FF] rounded-full"></div><p>Artificial Intelligence</p></a>
+          <h2 className="text-[1.3rem] lgtablet:text-[1.6rem] leading-7 mb-[5px]">Mastering ChatGPT Blog Creation: Dos and Don'ts for SaaS Marketing Managers</h2>
+          <p className="text-[#cfcece] hidden lgtablet:block">By <a href="">Joseph Olu</a></p>
+        </div>
+        </div>
+        
+        <div className="w-[100%] max-h-[450px] bg-absoluteWhite rounded-[30px] grid grid-cols-1 laptop:col-span-8 overflow-hidden relative text-absoluteWhite">
+          <img src={blogImg5} alt="" />
+          <div className="w-[100%] grid absolute bottom-0 px-[30px] py-[15px] shadow-lg backdrop-blur-md backdrop-saturate-[180%] bg-[rgba(0,0,0,0.3)] ">
+          <a href="" className="flex items-center gap-[10px] p-[5px] w-fit bg-[#F1ECFF] text-[#6C40FF] rounded-3xl mb-[5px]"><div className="h-[16px] w-[16px] bg-[#6C40FF] rounded-full"></div><p>Artificial Intelligence</p></a>
+          <h2 className="text-[1.3rem] lgtablet:text-[1.6rem] leading-7 mb-[5px]">Mastering ChatGPT Blog Creation: Dos and Don'ts for SaaS Marketing Managers</h2>
+          <p className="text-[#cfcece] hidden lgtablet:block">By <a href="">Joseph Olu</a></p>
+        </div>
+        </div>
+      </div>
 
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing</h2>
-
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita laboriosam incidunt iusto minus? Fugiat dolore repellat aspernatur temporibus, quasi corrupti cupiditate repellendus iste tempora! Ducimus voluptatibus neque nesciunt cumque incidunt quam pariatur, vitae, quod, nemo optio iste deleniti natus. Laboriosam non assumenda, omnis dolorum libero inventore, exercitationem minima repudiandae, earum ipsa excepturi dolor? Facere voluptas maiores, itaque natus id dignissimos inventore, vero quis est dolorum libero fuga labore non iure sit explicabo odit? Quam eos eaque eligendi. Est repellendus at sint, a amet officia fuga libero quisquam, quae, nesciunt doloremque.</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet totam nulla nisi accusantium, sapiente, quod unde, neque ullam rem possimus labore tenetur libero laboriosam obcaecati nesciunt quasi perspiciatis ratione reprehenderit. Fugiat doloremque, aliquam voluptatum quo saepe pariatur. Magnam quia eos quisquam iusto corrupti distinctio obcaecati molestias, quibusdam ea. Commodi, nesciunt? Similique provident alias modi officia illo veniam nam aut! Facere expedita sunt ullam dolorem in cupiditate delectus adipisci, quod quas?</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sunt id repellendus dolore nobis quae porro voluptas, autem maiores natus excepturi cumque recusandae. Eligendi nulla a quisquam voluptas quos eum modi vel eos id assumenda quia quam sint dolorum veritatis perspiciatis quod magnam itaque ut possimus, sed consectetur cupiditate non? Perspiciatis quisquam eveniet, ratione doloremque, esse officiis eius consequatur ipsum sint aliquid facilis nisi mollitia ut est iste. Minima, odit sunt nobis totam porro minus ipsa! Harum, maiores. Unde magnam obcaecati ducimus, alias expedita optio minima consequatur voluptates reprehenderit tempore soluta vitae voluptas labore aut explicabo consectetur id enim totam.</p>
-
-        <h2>Lorem ipsum dolor sit amet consectetur.</h2>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, aliquid est vel porro cum, neque fuga enim provident nam vitae fugiat odio velit voluptatibus ullam eius perferendis optio repellat consectetur! Optio nesciunt, numquam non, officia ullam debitis, molestias quas vitae consequatur cum in? Possimus vero, consequatur aut aliquam earum, soluta fugit tenetur facilis vitae totam consequuntur exercitationem quam atque quis neque tempora. Impedit magni similique necessitatibus quibusdam unde, iusto dicta voluptatum dolore enim accusamus eos nulla ipsa nesciunt facilis debitis!</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam nesciunt modi molestias nostrum cum quam praesentium voluptatem quibusdam. Ut dicta iure quae assumenda a fugit repudiandae, quas perferendis id consectetur animi esse omnis, cum ipsa debitis. Eligendi reprehenderit doloremque rerum, soluta repudiandae, autem at corporis culpa fugiat, sapiente molestias ut possimus officiis. Illum voluptatem at necessitatibus culpa, voluptas cum laborum, magnam voluptates in repellat aperiam fugiat esse laudantium molestias hic quam nesciunt. Quasi nesciunt fugiat cum, hic illo minus illum? Ducimus odit eveniet accusantium rerum?</p>
-
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis nemo magnam laborum sequi nisi recusandae quae, debitis expedita minus voluptate autem. Deserunt, similique. Illum saepe ipsa excepturi aperiam obcaecati magnam? Vel veritatis mollitia eligendi magni rem culpa eaque delectus, quas molestiae porro reprehenderit ab soluta sint ut blanditiis. Nostrum quidem inventore quos quia recusandae corporis, id fuga velit placeat soluta odio dolorem ratione, dignissimos iusto saepe, eveniet libero culpa architecto ea perspiciatis maiores? Sit ullam quidem voluptates quos porro dolor sint, optio inventore explicabo quasi rem, deleniti consequuntur voluptatum placeat sed adipisci. Placeat aperiam harum voluptas unde laboriosam nihil dicta accusantium. Natus labore quam voluptatem velit sapiente debitis cum corrupti.</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit reiciendis harum odio officia consequatur perferendis expedita qui esse earum vel necessitatibus reprehenderit molestias rem eum labore unde doloribus, ullam fugiat voluptatibus repellendus praesentium deserunt facilis aperiam id? Velit sint, facere, vero qui veniam cupiditate porro illo delectus alias voluptatibus enim est incidunt iste ab hic minima deleniti blanditiis tempora nesciunt laborum nihil adipisci illum, tempore recusandae. Amet molestias quasi vitae assumenda accusamus harum est quas, ducimus doloribus illo ea labore nemo praesentium. Commodi autem dolorum enim sunt debitis mollitia atque magni voluptatum est sapiente sit, aliquid facere laboriosam aliquam repellendus numquam distinctio, sequi aut blanditiis.</p>
-
-        <h2>Conclusion</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In incidunt quae quidem rem ratione facere cupiditate quo. Illo inventore, ducimus sed, quas unde doloremque iste eius maiores cum sint iusto.</p>
-      </article>
     </Fragment>
   )
 }
